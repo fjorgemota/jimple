bef2ad82d5551a6327fb6591eb94f05ba267979a
Fernando Jorge Mota <f.j.mota13@gmail.com>
2016-02-25 23:52:30 -0300

    Try to test on Chrome on Travis using new configuration on .travis.yml

--- karma.config.js
+++ karma.config.js
@@ -64,7 +64,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'PhantomJS'],
+    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
 
 
     // Continuous Integration mode
