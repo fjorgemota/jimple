35989bd8fc5709708e63b191d19b8ea163f24e1e
Fernando Jorge Mota <f.j.mota13@gmail.com>
2016-03-01 12:14:35 -0300

    Update Tonic Example so we can fix lint error on Codacy

--- tonicExample.js
+++ tonicExample.js
@@ -1,3 +1,4 @@
 var Jimple = require('jimple');
 var container = new Jimple();
 // Insert your container's configuration below:
+container.set("cookie_name", "SESSION_ID"); // An example of parameter, feel free to remove if you want
