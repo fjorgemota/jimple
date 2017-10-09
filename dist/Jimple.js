f0b2b7984f906cb10f2a123779ccbee96e31ad96
Fernando Jorge Mota <f.j.mota13@gmail.com>
2017-10-08 23:58:35 -0300

    Update distributable versions of the library

--- dist/Jimple.js
+++ dist/Jimple.js
@@ -50,14 +50,13 @@
     function isPlainObject(value) {
         if (Object.prototype.toString.call(value) !== '[object Object]') {
             return false;
-        } else {
-            var prototype = Object.getPrototypeOf(value);
-            return prototype === null || prototype === Object.prototype;
         }
+        var prototype = Object.getPrototypeOf(value);
+        return prototype === null || prototype === Object.prototype;
     }
 
     function checkDefined(container, key) {
-        assert(container.has(key), "Identifier '" + key + "' is not defined.");
+        assert(container.has(key), "Identifier \"" + key + "\" is not defined.");
     }
 
     function addFunctionTo(set, fn) {
@@ -66,35 +65,53 @@
         return fn;
     }
 
+    /**
+     * The Jimple Container class
+     *
+     * @public
+     */
+
     var Jimple = function () {
+        /**
+         * Create a Jimple Container.
+         * @param {Object?} [values] - An optional object whose keys and values will be associated in the container at initialization
+         */
         function Jimple(values) {
             _classCallCheck(this, Jimple);
 
-            this.items = {};
-            this.instances = new Map();
-            this.factories = new Set();
-            this.protected = new Set();
+            this._items = {};
+            this._instances = new Map();
+            this._factories = new Set();
+            this._protected = new Set();
             values = isPlainObject(values) ? values : {};
             Object.keys(values).forEach(function (key) {
                 this.set(key, values[key]);
             }, this);
         }
+        /**
+         * Return the specified parameter or service. If the service is not built yet, this function will construct the service
+         * injecting all the dependencies needed in it's definition so the returned object is totally usable on the fly.
+         * @param {string} key - The key of the parameter or service to return
+         * @return {*} The object related to the service or the value of the parameter associated with the key informed
+         * @throws If the key does not exist
+         */
+
 
         _createClass(Jimple, [{
             key: "get",
             value: function get(key) {
                 checkDefined(this, key);
-                var item = this.items[key];
+                var item = this._items[key];
                 var obj = void 0;
                 if (isFunction(item)) {
-                    if (this.protected.has(item)) {
+                    if (this._protected.has(item)) {
                         obj = item;
-                    } else if (this.instances.has(item)) {
-                        obj = this.instances.get(item);
+                    } else if (this._instances.has(item)) {
+                        obj = this._instances.get(item);
                     } else {
                         obj = item(this);
-                        if (!this.factories.has(item)) {
-                            this.instances.set(item, obj);
+                        if (!this._factories.has(item)) {
+                            this._instances.set(item, obj);
                         }
                     }
                 } else {
@@ -104,42 +121,42 @@
             }
         }, {
             key: "set",
-            value: function set(key, val) {
-                this.items[key] = val;
+            value: function set(key, value) {
+                this._items[key] = value;
             }
         }, {
             key: "has",
             value: function has(key) {
-                return this.items.hasOwnProperty(key);
+                return this._items.hasOwnProperty(key);
             }
         }, {
             key: "factory",
             value: function factory(fn) {
-                return addFunctionTo(this.factories, fn);
+                return addFunctionTo(this._factories, fn);
             }
         }, {
             key: "protect",
             value: function protect(fn) {
-                return addFunctionTo(this.protected, fn);
+                return addFunctionTo(this._protected, fn);
             }
         }, {
             key: "keys",
             value: function keys() {
-                return Object.keys(this.items);
+                return Object.keys(this._items);
             }
         }, {
             key: "extend",
             value: function extend(key, fn) {
                 checkDefined(this, key);
-                var originalItem = this.items[key];
-                assert(isFunction(originalItem) && this.protected.has(originalItem) === false, "Identifier '" + key + "' does not contain a service definition");
+                var originalItem = this._items[key];
+                assert(isFunction(originalItem) && this._protected.has(originalItem) === false, "Identifier '" + key + "' does not contain a service definition");
                 assert(isFunction(fn), "The 'new' service definition for '" + key + "' is not a invokable object.");
-                this.items[key] = function (app) {
+                this._items[key] = function (app) {
                     return fn(originalItem(app), app);
                 };
-                if (this.factories.has(originalItem)) {
-                    this.factories.delete(originalItem);
-                    this.factories.add(this.items[key]);
+                if (this._factories.has(originalItem)) {
+                    this._factories.delete(originalItem);
+                    this._factories.add(this._items[key]);
                 }
             }
         }, {
@@ -151,7 +168,57 @@
             key: "raw",
             value: function raw(key) {
                 checkDefined(this, key);
-                return this.items[key];
+                return this._items[key];
+            }
+        }], [{
+            key: "proxy",
+            value: function proxy(values) {
+                assert(typeof Proxy !== "undefined", "The actual environment does not support ES6 Proxy");
+                var container = new this(values);
+                var hasValues = {};
+                var methods = ["set", "get", "raw", "extend", "protect", "factory", "keys", "has", "register"];
+                methods.forEach(function (key) {
+                    return hasValues[key] = true;
+                });
+                container.keys().forEach(function (key) {
+                    return hasValues[key] = true;
+                });
+                var result = new Proxy(hasValues, {
+                    get: function get(obj, key) {
+                        var value = methods.includes(key) ? container[key].bind(container) : container.get(key);
+                        if (key === "set") {
+                            return function (k, val) {
+                                obj[k] = true;
+                                return value(k, val);
+                            };
+                        }
+                        return value;
+                    },
+                    set: function set(obj, key, value) {
+                        assert(!methods.includes(key), "The key \"" + key + "\" isn't valid because it's the name of a method of the container");
+                        obj[key] = true;
+                        container.set(key, value);
+                        return true;
+                    },
+                    ownKeys: function ownKeys(obj) {
+                        return container.keys();
+                    },
+                    has: function has(obj, key) {
+                        return container.has(key);
+                    },
+                    getOwnPropertyDescriptor: function getOwnPropertyDescriptor(obj, key) {
+                        if (!obj[key]) {
+                            return undefined;
+                        }
+                        return {
+                            'configurable': !container[key],
+                            'writable': !container[key],
+                            'enumerable': !container[key],
+                            'value': container.has(key) ? container.get(key) : undefined
+                        };
+                    }
+                });
+                return result;
             }
         }]);
 
