a863ca4e770cee5ac0bb0966a5364a9868712eab
Fernando Jorge Mota <contato@fjorgemota.com>
2025-11-13 10:12:37 -0300

    Update dev dependencies

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vitest/config";
+import { playwright } from "@vitest/browser-playwright"
 
 export default defineConfig({
   test: {
     browser: {
       enabled: true,
-      provider: "playwright",
+      provider: playwright(),
       headless: true,
       // https://vitest.dev/guide/browser/playwright
       instances: [{ browser: "chromium" }, { browser: "firefox" }],
